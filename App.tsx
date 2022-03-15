4b2143d18d0698fbdce53a3e450ba7d14a51b853
chuwong35122 <chuwong35122@gmail.com>
2022-03-15 22:51:18 +0700

    feat: add Poppins fonts

--- App.tsx
+++ App.tsx
@@ -4,7 +4,38 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import MainNavigation from './src/navigation/MainNavigation';
 
 const theme = extendTheme({
-  main: {}
+  main: {},
+  fontConfig: {
+    Poppins: {
+      300: {
+        normal: 'Poppins-Light',
+        italic: 'Poppins-LightItalic'
+      },
+      400: {
+        normal: 'Poppins-Regular',
+        italic: 'Poppins-Italic'
+      },
+      500: {
+        normal: 'Poppins-Medium',
+        italic: 'Poppins-MediumItalic'
+      },
+      600: {
+        normal: 'Poppins-SemiBold',
+        italic: 'Poppins-SemiBoldItalic'
+      },
+      700: {
+        normal: 'Poppins-Bold',
+        italic: 'Poppins-BoldItalic'
+      }
+    }
+  },
+
+  // Make sure values below matches any of the keys in `fontConfig`
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins',
+    mono: 'Poppins'
+  }
 });
 
 const App = () => {
