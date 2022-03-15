c6c8251176edaf9a68b4125da875c5c79ea1605b
chuwong35122 <chuwong35122@gmail.com>
2022-03-15 22:17:11 +0700

    feat: skeleton UI for ModuleAlertCard with shadow

--- src/components/molecules/ModuleAlertCard.tsx
+++ src/components/molecules/ModuleAlertCard.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet } from 'react-native';
-import { View, Text, Box, Divider, VStack, HStack } from 'native-base';
+import { View, Text, Divider, VStack, HStack } from 'native-base';
+import { Shadow } from 'react-native-shadow-2';
 import moment from 'moment';
 import React from 'react';
 
@@ -17,52 +18,54 @@ const ModuleAlertCard = ({
   description
 }: ModuleAlertCardProps) => {
   return (
-    <Box w="96" padding="4" borderColor="gray.100" style={styles.cardContainer}>
-      <VStack>
-        <HStack justifyContent="space-between">
-          <Text color="darkBlue.600" fontSize="16">
-            {moduleName}
-          </Text>
-          <Text color="darkBlue.600" fontSize="16">
-            {moment(time).format('hh:mm')}
-          </Text>
-        </HStack>
-        <View>
-          <Text fontSize="2xl" bold>
-            {title}
-          </Text>
-          {description && <Text fontSize="xl">{description}</Text>}
-        </View>
-        <HStack w="full" justifyContent="space-between" my={4}>
-          <View flex={2} alignItems="center">
-            <Text fontSize="2xl" color="blue.500">
-              I did it
+    <Shadow distance={8}>
+      <View w="82%" px="4" py="2" style={styles.card}>
+        <VStack>
+          <HStack justifyContent="space-between">
+            <Text color="darkBlue.600" fontSize="16">
+              {moduleName}
             </Text>
-          </View>
-          <Divider orientation="vertical" mx="6" bg="gray.200" thickness={2} />
-          <View flex={2} alignItems="center">
-            <Text fontSize="2xl" color="blue.500">
-              Dismiss
+            <Text color="darkBlue.600" fontSize="16">
+              {moment(time).format('hh:mm')}
             </Text>
+          </HStack>
+          <View>
+            <Text fontSize="2xl" bold>
+              {title}
+            </Text>
+            {description && <Text fontSize="xl">{description}</Text>}
           </View>
-        </HStack>
-      </VStack>
-    </Box>
+          <HStack w="full" justifyContent="space-between" my={1}>
+            <View flex={2} alignItems="center">
+              <Text fontSize="xl" color="blue.500">
+                I did it
+              </Text>
+            </View>
+            <Divider
+              orientation="vertical"
+              mx="6"
+              bg="gray.200"
+              thickness={2}
+            />
+            <View flex={2} alignItems="center">
+              <Text fontSize="xl" color="blue.500">
+                Dismiss
+              </Text>
+            </View>
+          </HStack>
+        </VStack>
+      </View>
+    </Shadow>
   );
 };
 
 export default ModuleAlertCard;
 
 const styles = StyleSheet.create({
-  cardContainer: {
-    borderRadius: 10,
-    shadowColor: '#020202',
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.1,
-    shadowRadius: 5,
-    elevation: 1
+  card: {
+    borderBottomLeftRadius: 6,
+    borderBottomRightRadius: 6,
+    borderTopLeftRadius: 6,
+    borderTopRightRadius: 6
   }
 });
