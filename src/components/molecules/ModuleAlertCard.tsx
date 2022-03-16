cee212bc20606ba61ed8ea59bc1cdb27768e9dae
chuwong35122 <chuwong35122@gmail.com>
2022-03-16 20:50:47 +0700

    deps: remove react-native-shadow-2

--- src/components/molecules/ModuleAlertCard.tsx
+++ src/components/molecules/ModuleAlertCard.tsx
@@ -1,6 +1,5 @@
 import { StyleSheet } from 'react-native';
 import { View, Text, Divider, VStack, HStack } from 'native-base';
-import { Shadow } from 'react-native-shadow-2';
 import moment from 'moment';
 import React from 'react';
 
@@ -18,37 +17,35 @@ const ModuleAlertCard = ({
   description
 }: ModuleAlertCardProps) => {
   return (
-    <Shadow distance={8}>
-      <VStack w="96" px="4" py="2" style={styles.card}>
-        <HStack justifyContent="space-between">
-          <Text color="darkBlue.600" fontSize="16">
-            {moduleName}
+    <VStack w="96" px="4" py="2" backgroundColor="#fff" style={styles.card}>
+      <HStack justifyContent="space-between">
+        <Text color="darkBlue.600" fontSize="16">
+          {moduleName}
+        </Text>
+        <Text color="darkBlue.600" fontSize="16">
+          {moment(time).format('hh:mm')}
+        </Text>
+      </HStack>
+      <View>
+        <Text fontSize="2xl" fontWeight="500">
+          {title}
+        </Text>
+        {description && <Text fontSize="xl">{description}</Text>}
+      </View>
+      <HStack justifyContent="space-between" mt={4} mb={1}>
+        <View flex={2} alignItems="center">
+          <Text fontSize="xl" color="blue.500">
+            I did it
           </Text>
-          <Text color="darkBlue.600" fontSize="16">
-            {moment(time).format('hh:mm')}
-          </Text>
-        </HStack>
-        <View>
-          <Text fontSize="2xl" fontWeight="500">
-            {title}
-          </Text>
-          {description && <Text fontSize="xl">{description}</Text>}
         </View>
-        <HStack justifyContent="space-between" mt={4} mb={1}>
-          <View flex={2} alignItems="center">
-            <Text fontSize="xl" color="blue.500">
-              I did it
-            </Text>
-          </View>
-          <Divider orientation="vertical" mx="6" bg="gray.200" thickness={2} />
-          <View flex={2} alignItems="center">
-            <Text fontSize="xl" color="blue.500">
-              Dismiss
-            </Text>
-          </View>
-        </HStack>
-      </VStack>
-    </Shadow>
+        <Divider orientation="vertical" mx="6" bg="gray.200" thickness={2} />
+        <View flex={2} alignItems="center">
+          <Text fontSize="xl" color="blue.500">
+            Dismiss
+          </Text>
+        </View>
+      </HStack>
+    </VStack>
   );
 };
 
@@ -59,6 +56,14 @@ const styles = StyleSheet.create({
     borderBottomLeftRadius: 6,
     borderBottomRightRadius: 6,
     borderTopLeftRadius: 6,
-    borderTopRightRadius: 6
+    borderTopRightRadius: 6,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2
   }
 });
