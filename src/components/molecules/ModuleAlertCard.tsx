bfab7a40170d72205db608f4c38d6ccc648a6b08
chuwong35122 <chuwong35122@gmail.com>
2022-03-16 22:34:23 +0700

    fix: alignment and size of ModuleAlertCard

--- src/components/molecules/ModuleAlertCard.tsx
+++ src/components/molecules/ModuleAlertCard.tsx
@@ -17,35 +17,37 @@ const ModuleAlertCard = ({
   description
 }: ModuleAlertCardProps) => {
   return (
-    <VStack w="96" px="4" py="2" backgroundColor="#fff" style={styles.card}>
-      <HStack justifyContent="space-between">
-        <Text color="darkBlue.600" fontSize="16">
-          {moduleName}
-        </Text>
-        <Text color="darkBlue.600" fontSize="16">
-          {moment(time).format('hh:mm')}
-        </Text>
-      </HStack>
-      <View>
-        <Text fontSize="2xl" fontWeight="500">
-          {title}
-        </Text>
-        {description && <Text fontSize="xl">{description}</Text>}
-      </View>
-      <HStack justifyContent="space-between" mt={4} mb={1}>
-        <View flex={2} alignItems="center">
-          <Text fontSize="xl" color="blue.500">
-            I did it
+    <View>
+      <VStack px="4" py="2" backgroundColor="#fff" style={styles.card}>
+        <HStack justifyContent="space-between">
+          <Text color="darkBlue.600" fontSize="16">
+            {moduleName}
           </Text>
-        </View>
-        <Divider orientation="vertical" mx="6" bg="gray.200" thickness={2} />
-        <View flex={2} alignItems="center">
-          <Text fontSize="xl" color="blue.500">
-            Dismiss
+          <Text color="darkBlue.600" fontSize="16">
+            {moment(time).format('hh:mm')}
           </Text>
+        </HStack>
+        <View>
+          <Text fontSize="2xl" fontWeight="500">
+            {title}
+          </Text>
+          {description && <Text fontSize="xl">{description}</Text>}
         </View>
-      </HStack>
-    </VStack>
+        <HStack justifyContent="space-between" mt={4} mb={1}>
+          <View flex={2} alignItems="center">
+            <Text fontSize="xl" color="blue.500">
+              I did it
+            </Text>
+          </View>
+          <Divider orientation="vertical" mx="6" bg="gray.200" thickness={2} />
+          <View flex={2} alignItems="center">
+            <Text fontSize="xl" color="blue.500">
+              Dismiss
+            </Text>
+          </View>
+        </HStack>
+      </VStack>
+    </View>
   );
 };
 
