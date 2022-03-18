2a524f17c83962548da99ac7c15833ebb745a76a
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2022-03-18 22:22:59 +0700

    feat: add i18next example

--- src/components/atoms/Spacer.tsx
+++ src/components/atoms/Spacer.tsx
@@ -6,6 +6,14 @@ type SpacerProps = {
   w?: number;
 };
 
+/**
+ *
+ * @param h - height of the empty space
+ * @param w -width of the empty space
+ * @description
+ * This is an empty box with default size of 16 in height and 8 in width.
+ * You may customize the size as you wish using the w and h parameters.
+ */
 const Spacer = ({ h, w }: SpacerProps) => {
   return <View style={{ height: h ?? 16, width: w ?? 8 }} />;
 };
