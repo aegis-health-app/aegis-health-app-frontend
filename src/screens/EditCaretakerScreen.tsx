4cdb4e0d4c5836ff36d2d3f1ac903dffb97c984b
supat <supatachbeam@gmail.com>
2022-03-20 19:34:35 +0700

    WIP: setup temp params for caretakers card

--- src/screens/EditCaretakerScreen.tsx
+++ src/screens/EditCaretakerScreen.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
+<<<<<<< HEAD
 import { ScrollView, View } from 'native-base';
+=======
+import { View, Text } from 'native-base';
+>>>>>>> 416ec95 (WIP: setup temp params for caretakers card)
 import RemButton from '../components/atoms/RemButton';
 import Divider from '../components/atoms/Divider';
 import InputBox from '../components/atoms/Input';
 import ProfileInfoCard from '../components/organisms/ProfileInfoCard';
 import KeyboardAvoidingView from '../components/atoms/KeyboardAvoidingView';
 
-const EditCaretakerScreen = () => {
+const EditCaretakerScreen = ({route, navigation}) => {
+
+  // WIP
+  const {itemId} = route.params;
+
   const { t } = useTranslation();
   return (
     <KeyboardAvoidingView>
       <ScrollView>
         <View bgColor="#FAFAFA">
-          <ProfileInfoCard name="Somying Muangyim" gender="Female" bdate="08/02/1917" phone="090909090"/>
+          <ProfileInfoCard name={itemId} gender="Female" bdate="08/02/1917" phone="090909090"/>
           <Divider />
+<<<<<<< HEAD
           <View paddingX={5}>
             <InputBox name="เปลี่ยนชื่อผู้ใช้" />
           </View>
+=======
+          <InputBox name={t('106')} placeholder={t('106')}/>
+          <Text></Text>
+>>>>>>> 416ec95 (WIP: setup temp params for caretakers card)
           <Divider />
-          <RemButton />
+          <RemButton name={itemId}/>
         </View>
       </ScrollView>
     </KeyboardAvoidingView>
