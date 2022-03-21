ed93144b11ba7b4d325f2b88989ab3911f4affda
SPK137 <o.sippakorn@gmail.com>
2022-03-21 13:18:51 +0700

    feat: add easy import for images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,8 @@
+const images = {
+  authBanner: require('./authBanner.png'),
+  scanMe: require('./scanme.png'),
+  sompochImg: require('./sompoch.png'),
+  somyingImg: require('./somying.png')
+};
+
+export default images;
