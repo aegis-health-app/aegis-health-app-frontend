9d42c4200b8a630580bad96e58e94c0568ba0cf0
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2022-03-22 23:12:38 +0700

    feat: change password UI and form validation

--- src/components/atoms/InputField.tsx
+++ src/components/atoms/InputField.tsx
@@ -2,14 +2,17 @@ import { Input as NativeBaseInput } from 'native-base';
 import React from 'react';
 
 type InputProps = {
-  onBlur: any;
+  onBlur: (e: any) => void;
   placeholder: string;
   onChangeText: any;
   value: string | undefined;
+  InputRightElement?: any;
+  type?: string;
 };
 
 const Input = (props: InputProps) => {
-  const { onBlur, placeholder, onChangeText, value } = props;
+  const { onBlur, placeholder, onChangeText, value, type, InputRightElement } =
+    props;
   return (
     <NativeBaseInput
       borderColor="#ACB5BD"
@@ -21,6 +24,8 @@ const Input = (props: InputProps) => {
       placeholder={placeholder}
       onChangeText={onChangeText}
       value={value}
+      type={type}
+      InputRightElement={InputRightElement}
     />
   );
 };
--- src/dto/Password.ts
+++ src/dto/Password.ts
@@ -2,6 +2,18 @@ import * as Yup from 'yup';
 
 export const changePasswordSchema = Yup.object({
   oldPassword: Yup.string().required('Old Password cannot be left blank'),
-  newPassword: Yup.string().required('Password cannot be left blank'),
-  repeatNewPassword: Yup.string().required('Password cannot be left blank')
+  newPassword: Yup.string()
+    .required('Password cannot be left blank')
+    .oneOf([Yup.ref('repeatNewPassword'), null], 'Passwords must match')
+    .notOneOf(
+      [Yup.ref('oldPassword'), null],
+      'New password cannot be the same as the old password'
+    ),
+  repeatNewPassword: Yup.string()
+    .required('Password cannot be left blank')
+    .oneOf([Yup.ref('newPassword'), null], 'Passwords must match')
+    .notOneOf(
+      [Yup.ref('oldPassword'), null],
+      'New password cannot be the same as the old password'
+    )
 });
