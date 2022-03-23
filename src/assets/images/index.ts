f8e1148b2ab1e9accc7cb41c2e523405850651cb
Varisss <varisthehacker@hotmail.com>
2022-03-23 23:04:48 +0700

    feat: added i18n for plan selection

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,7 +2,9 @@ const images = {
   authBanner: require('./authBanner.png'),
   scanMe: require('./scanme.png'),
   sompochImg: require('./sompoch.png'),
-  somyingImg: require('./somying.png')
+  somyingImg: require('./somying.png'),
+  selfCare: require('./selfCare.png'),
+  elderlyCare: require('./elderlyCare.png')
 };
 
 export default images;
