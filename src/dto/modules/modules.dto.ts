945542595cfef749491cb64bbbc494935de66789
chuwong35122 <chuwong35122@gmail.com>
2022-03-24 16:45:05 +0700

    fix: update user typings

--- src/dto/modules/modules.dto.ts
+++ src/dto/modules/modules.dto.ts
@@ -1,5 +1,3 @@
-import { BloodType } from '../../interfaces/User';
-
 export enum ModuleEnum {
   reminder = 1,
   healthRecords,
@@ -12,21 +10,6 @@ export type ModuleId = 1 | 2 | 3 | 4;
 export interface Module {
   moduleid: ModuleId;
   mname: string;
-}
-
-export interface ElderlyUser {
-  fname: string;
-  lname: string;
-  dname: string;
-  gender: string;
-  bday: string;
-  bloodType: BloodType;
-  healthCondition: string | null;
-  personalMedication: string | null;
-  allergy: string | null;
-  vaccine: string | null;
-  phone: string;
-  listModuleid: ModuleId[];
 }
 
 export interface ElderlyHomeProfile {
--- src/dto/modules/user.dto.ts
+++ src/dto/modules/user.dto.ts
@@ -1,18 +1,32 @@
-import { BloodType } from '../../interfaces/User';
+import { ModuleId } from './modules.dto';
 
+export type BloodType = 'N/A' | 'A' | 'B' | 'O' | 'AB' | string;
+
+export enum Gender {
+  female = 'F',
+  male = 'M'
+}
 export interface User {
   imageid: string;
   fname: string;
   lname: string;
   dname: string;
   bday: string;
-  gender: string;
+  gender: 'F' | 'M';
   isElderly: boolean;
-  healthCondition: string | null;
-  bloodType: BloodType;
-  personalMedication: string | null;
-  allergy: string | null;
-  vaccine: string | null;
-  phone: string;
   uid: number;
+}
+
+export interface Caretaker extends User {
+  listElderly: Elderly[];
+}
+
+export interface Elderly extends User {
+  listModuleid: ModuleId[];
+  healthCondition?: string | null;
+  bloodType: BloodType;
+  personalMedication?: string | null;
+  allergy?: string | null;
+  vaccine?: string | null;
+  phone?: string;
 }
