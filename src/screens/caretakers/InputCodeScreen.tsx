ea89acbd707ea07802a536e9809ed2149e906729
supat <supatachbeam@gmail.com>
2022-03-24 15:36:38 +0700

    feat: added form validation for code input

--- src/screens/caretakers/InputCodeScreen.tsx
+++ src/screens/caretakers/InputCodeScreen.tsx
@@ -1,16 +1,51 @@
 import { View, Text, Button } from 'native-base';
-import React from 'react';
-import InputBox from '../../components/atoms/Input';
+import React, { useEffect, useState} from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../navigation/types';
 import { useTranslation } from 'react-i18next';
+import TextInput from '../../components/atoms/TextInput';
+import { useForm, useFormState } from 'react-hook-form';
+import * as Yup from 'yup';
+import { useYupValidationResolver } from '../../hooks/useYupValidationResolver';
 
 const InputCodeScreen = () => {
+
   const { t } = useTranslation();
 
-    const navigation =
-    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  const codeSchema = Yup.object({
+    // elderlyCode: Yup.string().oneOf(codes, t('userLink.invalidCode'))
+    elderlyCode: Yup.string().max(6,'').min(6,'')
+  })
+
+  const {
+    control,
+    formState: { errors },
+    watch
+  } = useForm({ resolver:  useYupValidationResolver(codeSchema), mode: "all"});
+
+
+  // todo: get codes from backend
+  const [codes, setCodes] = useState(['000000','AAAAAA'])
+  const watchElderlyCode = watch();
+  const [isCodeValid, setIsCodeValid] = useState(true)
+
+  const navigation =
+  useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+  const { isValid} = useFormState({control})
+
+  useEffect(() => {
+    console.log(isValid, codes.includes(watchElderlyCode["elderlyCode"]))
+  })
+
+  const handleSubmit = () => {
+    if(codes.includes(watchElderlyCode["elderlyCode"])) {
+      navigation.navigate('ConfirmConnectScreen')
+      setIsCodeValid(true)
+    }
+    else setIsCodeValid(false)
+  }
 
   return (
     <View my="2">
@@ -31,12 +66,25 @@ const InputCodeScreen = () => {
             </Button>
             <Button borderRadius="0" width="45%" onPress={() => navigation.navigate('InputCodeScreen')}>{t('userLink.enterCode')}</Button>
         </Button.Group>
-        <View paddingX={5}>
-        <InputBox name={t('userLink.enterElderlyCode')}/>
+        <View mt={2} paddingX={5}>
+        <TextInput
+            label={t('userLink.enterElderlyCode')}
+            placeholder='XXXXXX'
+            name="elderlyCode"
+            control={control}
+            errors={errors}
+            mb={2}
+          />
         </View>
-        <View my="2" flexDir="row" justifyContent="center">
-            <Button width="90%" onPress={() => navigation.navigate('ConfirmConnectScreen')}>{t('userLink.enterButton')}</Button>
-            </View>
+        {isCodeValid? null: 
+          <View flexDir="row">
+            <Text ml="5" color="red.500">
+              {t('userLink.invalidCode')}
+            </Text>
+          </View>}
+        <View my="4" flexDir="row" justifyContent="center">
+            <Button isDisabled={!isValid} width="90.5%" onPress={() => handleSubmit()}>{t('userLink.enterButton')}</Button>
+        </View>
     </View>
   );
 };
