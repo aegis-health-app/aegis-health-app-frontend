e737341ecd9432535dad66b562800c1f7b0423e3
SPK137 <o.sippakorn@gmail.com>
2022-03-26 03:10:15 +0700

    feat: add signUp 1st stage

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,5 +1,13 @@
-import { Box, Button, HStack, ScrollView, Text, VStack } from 'native-base';
-import React, { useCallback, useState } from 'react';
+import {
+  Box,
+  Button,
+  HStack,
+  Pressable,
+  ScrollView,
+  Text,
+  VStack
+} from 'native-base';
+import React, { useCallback, useMemo, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Spacer from '../components/atoms/Spacer';
@@ -9,11 +17,13 @@ import TextInput, { PasswordTextInput } from '../components/atoms/TextInput';
 import { useTranslation } from 'react-i18next';
 import ControlledOTPInput from '../components/molecules/ControlledOTPInput';
 import useDimensions from '../hooks/useDimensions';
-import { Platform } from 'react-native';
+import { Platform, View } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { getFormattedDate } from '../utils/getFormattedDate';
 import { useSettings } from '../hooks/useSettings';
 import Divider from '../components/atoms/Divider';
+import FormDescription from '../components/atoms/FormDescription';
+import OTPTimerButton from '../components/atoms/OTPTimerButton';
 
 interface InformationList {
   label: string;
@@ -22,27 +32,20 @@ interface InformationList {
   type?: 'password' | 'birthDate' | 'gender' | 'bloodGroup';
 }
 
-const informationList: InformationList[] = [
-  { label: 'profile.name', name: 'name' },
-  { label: 'profile.lastName', name: 'lastName' },
-  { label: 'profile.displayName', name: 'displayName' },
-  { label: 'profile.birthDate', name: 'birthDate', type: 'birthDate' },
-  { label: 'profile.birthGender', name: 'birthGender', type: 'gender' },
-  { label: 'auth.password', name: 'password', type: 'password' },
-  { label: 'auth.confirmPassword', name: 'confirmPassword', type: 'password' }
-];
-
 const SignUpScreen = () => {
   const {
     control,
     formState: { errors },
-    handleSubmit
+    handleSubmit,
+    watch
   } = useForm();
 
   const { t } = useTranslation();
   const { ScreenHeight } = useDimensions();
 
   const { language } = useSettings();
+
+  const [signUpStage, setSignUpStage] = useState<number>(1);
 
   const [date, setDate] = useState(new Date(700938977));
   const [mode, setMode] = useState('date');
@@ -61,7 +64,33 @@ const SignUpScreen = () => {
     setMode('date');
   };
 
-  const onFormSubmit = useCallback(() => null, []);
+  const backToPreviousStage = useCallback(() => {
+    if (signUpStage > 1) setSignUpStage((prev) => prev - 1);
+  }, [signUpStage]);
+
+  const continueToNextStage = useCallback(() => {
+    if (signUpStage === 1) setSignUpStage((prev) => prev + 1);
+    if (signUpStage === 2) setSignUpStage((prev) => prev + 1);
+  }, [signUpStage]);
+
+  const informationList: InformationList[] = useMemo(
+    () => [
+      { label: 'profile.name', name: 'name' },
+      { label: 'profile.lastName', name: 'lastName' },
+      { label: 'profile.displayName', name: 'displayName' },
+      { label: 'profile.birthDate', name: 'birthDate', type: 'birthDate' },
+      { label: 'profile.birthGender', name: 'birthGender', type: 'gender' },
+      { label: 'profile.phoneNumber', name: 'phoneNumber' },
+      { label: 'auth.password', name: 'password', type: 'password' },
+      {
+        label: 'auth.confirmPassword',
+        name: 'confirmPassword',
+        type: 'password'
+      }
+    ],
+    []
+  );
+
 
   return (
     <SafeAreaView>
@@ -69,106 +98,121 @@ const SignUpScreen = () => {
         <VStack px={4} minH={ScreenHeight}>
           <FormHeader headerText={t('auth.signUp')} mt={16} />
           <Divider my={2} />
-          <FormHeader headerText={t('auth.signUp')} my={2} size={20} />
-          {informationList.map((info) => (
-            <Box mb={6} key={info.label}>
-              {!info.type && (
-                <TextInput
-                  label={`${t(info.label)} `}
-                  placeholder={t(info.placeholder || info.label)}
-                  name={info.name}
-                  control={control}
-                  errors={errors}
-                  hasRequiredStar
-                  isRequired
-                  errorMessage={t('error.isRequired', { name: t(info.label) })}
-                />
-              )}
+          {signUpStage === 1 && (
+            <View>
+              <FormHeader headerText={t('auth.generalInfo')} my={2} size={20} />
+              {informationList.map((info) => (
+                <Box mb={6} key={`${info.label}-${info.name}`}>
+                  {!info.type && (
+                    <TextInput
+                      label={`${t(info.label)} `}
+                      placeholder={t(info.placeholder || info.label)}
+                      name={info.name}
+                      control={control}
+                      errors={errors}
+                      hasRequiredStar
+                      isRequired
+                      errorMessage={t('error.isRequired', {
+                        name: t(info.label)
+                      })}
+                    />
+                  )}
 
-              {info.type === 'password' && (
-                <PasswordTextInput
-                  label={`${t(info.label)} `}
-                  placeholder={t(info.placeholder || info.label)}
-                  name={info.name}
-                  control={control}
-                  errors={errors}
-                  hasRequiredStar
-                  isRequired
-                  errorMessage={t('error.isRequired', { name: t(info.label) })}
-                />
-              )}
-              {info.type === 'birthDate' && (
-                <>
-                  <Text fontSize={16} mb={2}>
-                    {t('profile.birthDate')}
-                  </Text>
-                  {(Platform.OS === 'ios' || show) && (
-                    <HStack justifyContent="space-between" alignItems="center">
-                      {Platform.OS === 'ios' && (
-                        <Text w={100}>{getFormattedDate(date, language)}</Text>
+                  {info.type === 'password' && (
+                    <PasswordTextInput
+                      label={`${t(info.label)} `}
+                      placeholder={t(info.placeholder || info.label)}
+                      name={info.name}
+                      control={control}
+                      errors={errors}
+                      hasRequiredStar
+                      isRequired
+                      errorMessage={t('error.isRequired', {
+                        name: t(info.label)
+                      })}
+                    />
+                  )}
+
+                  {info.type === 'birthDate' && (
+                    <>
+                      <Text fontSize={16} mb={2}>
+                        {t('profile.birthDate')}
+                      </Text>
+                      {(Platform.OS === 'ios' || show) && (
+                        <HStack
+                          justifyContent="space-between"
+                          alignItems="center">
+                          {Platform.OS === 'ios' && (
+                            <Text w={100}>
+                              {getFormattedDate(date, language)}
+                            </Text>
+                          )}
+                          <DateTimePicker
+                            testID="dateTimePicker"
+                            value={date}
+                            onChange={onDateChange}
+                            mode={mode as any}
+                            style={
+                              Platform.OS === 'ios' ? { width: 124 } : null
+                            }
+                          />
+                        </HStack>
                       )}
-                      <DateTimePicker
-                        testID="dateTimePicker"
-                        value={date}
-                        onChange={onDateChange}
-                        mode={mode as any}
-                        style={Platform.OS === 'ios' ? { width: 124 } : null}
-                      />
-                    </HStack>
+                      {Platform.OS === 'android' && (
+                        <HStack
+                          justifyContent="space-between"
+                          alignItems="center">
+                          <Text w={100}>
+                            {getFormattedDate(date, language)}
+                          </Text>
+                          <Button
+                            variant="outline"
+                            onPress={() => showDatepicker()}>
+                            {t('userForm.editBirthdate')}
+                          </Button>
+                        </HStack>
+                      )}
+                    </>
                   )}
-                  {Platform.OS === 'android' && (
-                    <HStack justifyContent="space-between" alignItems="center">
-                      <Text w={100}>{getFormattedDate(date, language)}</Text>
-                      <Button
-                        variant="outline"
-                        onPress={() => showDatepicker()}>
-                        {t('userForm.editBirthdate')}
-                      </Button>
-                    </HStack>
-                  )}
-                </>
-              )}
-              {info.type === 'gender' && (
-                <>
-                  <Text fontSize={16} mb={2}>
-                    {t('profile.birthGender')}
-                  </Text>
-                  <HStack justifyContent="space-between" alignItems="center">
-                    <HStack justifyContent="flex-start">
-                      <Button
-                        minWidth={20}
-                        variant={gender === 'male' ? 'solid' : 'outline'}
-                        onPress={() => {
-                          setGender('male');
-                        }}>
-                        {t('userForm.male')}
-                      </Button>
-                      <Spacer />
-                      <Button
-                        minWidth={20}
-                        variant={gender === 'female' ? 'solid' : 'outline'}
-                        onPress={() => {
-                          setGender('female');
-                        }}>
-                        {t('userForm.female')}
-                      </Button>
-                    </HStack>
-                  </HStack>
-                </>
-              )}
-            </Box>
-          ))}
 
-          <Button w="full" onPress={handleSubmit(onFormSubmit)}>
-            {t('auth.continue')}
-          </Button>
-          <ControlledOTPInput
-            label={t('general.otp')}
-            name="otp"
-            errors={errors}
-            control={control}
-            isRequired
-          />
+                  {info.type === 'gender' && (
+                    <>
+                      <Text fontSize={16} mb={2}>
+                        {t('profile.birthGender')}
+                      </Text>
+                      <HStack
+                        justifyContent="space-between"
+                        alignItems="center">
+                        <HStack justifyContent="flex-start">
+                          <Button
+                            minWidth={20}
+                            variant={gender === 'male' ? 'solid' : 'outline'}
+                            onPress={() => {
+                              setGender('male');
+                            }}>
+                            {t('userForm.male')}
+                          </Button>
+                          <Spacer />
+                          <Button
+                            minWidth={20}
+                            variant={gender === 'female' ? 'solid' : 'outline'}
+                            onPress={() => {
+                              setGender('female');
+                            }}>
+                            {t('userForm.female')}
+                          </Button>
+                        </HStack>
+                      </HStack>
+                    </>
+                  )}
+                </Box>
+              ))}
+              <Button w="full" onPress={handleSubmit(continueToNextStage)}>
+                {t('auth.continue')}
+              </Button>
+            </View>
+          )}
+
           <Box flex={1} />
           <AuthFooter page={AuthType.SIGNUP} />
         </VStack>
