715149a032c555fe970aafea0750f4b1799be891
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2022-03-25 18:13:21 +0700

    chore: update UserContext

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,17 +1,23 @@
 import React, { useState, useEffect, createContext } from 'react';
+import useAsyncEffect from '../hooks/useAsyncEffect';
+import { useAuthentication } from '../hooks/useAuthentication';
 import { User } from './../dto/modules/user.dto';
 
 export interface UserContextProps {
   user: User | undefined;
   setUser: (value: User) => void;
   isElderly: boolean | undefined;
+  token: string;
+  setToken: (token: string) => Promise<void>;
 }
 
 export const UserContext = createContext({} as UserContextProps);
 
 const UserContextProvider = ({ ...props }) => {
+  const { setToken, getToken } = useAuthentication();
   const [user, setUser] = useState<User | undefined>(undefined);
   const [isElderly, setIsEldery] = useState<boolean>(false);
+  const [token, setUserToken] = useState<string>('');
 
   useEffect(() => {
     // fetching user from back-end
@@ -23,10 +29,17 @@ const UserContextProvider = ({ ...props }) => {
     }
   }, [user]);
 
+  useAsyncEffect(async () => {
+    const _token = (await getToken()) ?? '';
+    setUserToken(_token);
+  }, [getToken, setUserToken]);
+
   const value = {
     user,
     setUser,
-    isElderly
+    isElderly,
+    token,
+    setToken
   };
   return <UserContext.Provider value={value} {...props} />;
 };
