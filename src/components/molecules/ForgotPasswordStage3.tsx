576cae35dd84944493f64d16c1cce6dbdc9ac887
Varisss <varisthehacker@hotmail.com>
2022-03-25 16:33:45 +0700

    feat: add forgot password page

---
+++ src/components/molecules/ForgotPasswordStage3.tsx
@@ -0,0 +1,97 @@
+import { Box, Button, Icon, Text, VStack } from 'native-base';
+import React, { useState } from 'react';
+
+import AuthFooter, { AuthType } from '../atoms/AuthFooter';
+import FormHeader from '../atoms/FormHeader';
+import FormTitle from '../atoms/FormTitle';
+import TextInput from '../atoms/TextInput';
+import { useTranslation } from 'react-i18next';
+import Spacer from '../atoms/Spacer';
+import { StyleSheet, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Divider from '../atoms/Divider';
+
+type ForgotPasswordProps = {
+  control: any;
+  errors: any;
+  watch: any;
+};
+
+const ForgotPasswordStage3 = ({
+  control,
+  errors,
+  watch
+}: ForgotPasswordProps) => {
+  const [showNewPassword, setShowNewPassword] = useState<boolean>(false);
+  const [showRepeatPassword, setShowRepeatPassword] = useState<boolean>(false);
+  const { t } = useTranslation();
+  return (
+    <VStack px={2}>
+      <FormHeader headerText={t('changePassword.changePassword')} mt={10} />
+      <Divider />
+      <FormTitle titleText={t('changePassword.header')} />
+      <Spacer />
+      <Spacer />
+      <View>
+        {/* Old Password */}
+        <View style={styles.inputRow}>
+          <TextInput
+            label={t('changePassword.newPassword')}
+            placeholder={t('changePassword.newPassword')}
+            name="newPassword"
+            control={control}
+            errors={errors}
+            type={showNewPassword ? 'text' : 'password'}
+            InputRightElement={
+              <Icon
+                mr={2}
+                size="sm"
+                h="full"
+                as={Ionicons}
+                name={showNewPassword ? 'eye' : 'eye-off'}
+                color="muted.400"
+                onPress={() => setShowNewPassword(!showNewPassword)}
+              />
+            }
+          />
+        </View>
+        <Spacer />
+        <View style={styles.inputRow}>
+          <TextInput
+            label={t('changePassword.repeatNewPassword')}
+            placeholder={t('changePassword.repeatNewPassword')}
+            name="repeatNewPassword"
+            control={control}
+            errors={errors}
+            type={showRepeatPassword ? 'text' : 'password'}
+            InputRightElement={
+              <Icon
+                mr={2}
+                size="sm"
+                h="full"
+                as={Ionicons}
+                name={showRepeatPassword ? 'eye' : 'eye-off'}
+                color="muted.400"
+                onPress={() => setShowRepeatPassword(!showRepeatPassword)}
+              />
+            }
+          />
+        </View>
+        <Spacer />
+      </View>
+      <Button w="full" onPress={() => console.log('test')}>
+        {t('changePassword.changePassword')}
+      </Button>
+      <Box flex={1} />
+      <AuthFooter page={AuthType.SIGNUP} />
+    </VStack>
+  );
+};
+
+export default ForgotPasswordStage3;
+
+const styles = StyleSheet.create({
+  inputRow: {
+    minHeight: 95
+  }
+});
---
+++ src/screens/ForgotPasswordScreen.tsx
@@ -0,0 +1,54 @@
+import { ScrollView } from 'native-base';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { SafeAreaView } from 'react-native-safe-area-context';
+// import { useTranslation } from 'react-i18next';
+import ForgotPasswordStage1 from '../components/molecules/ForgotPasswordStage1';
+import ForgotPasswordStage2 from '../components/molecules/ForgotPasswordStage2';
+import ForgotPasswordStage3 from '../components/molecules/ForgotPasswordStage3';
+
+enum stages {
+  first = 1,
+  second = 2,
+  third = 3
+}
+
+const ForgotPasswordScreen = () => {
+  const {
+    control,
+    formState: { errors },
+    watch
+  } = useForm();
+  // const { t } = useTranslation();
+  const [stage, setStage] = useState(stages.first);
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        {stage === stages.first ? (
+          <ForgotPasswordStage1
+            control={control}
+            errors={errors}
+            watch={watch}
+            setStage={setStage}
+          />
+        ) : stage === stages.second ? (
+          <ForgotPasswordStage2
+            control={control}
+            errors={errors}
+            watch={watch}
+            setStage={setStage}
+          />
+        ) : (
+          <ForgotPasswordStage3
+            control={control}
+            errors={errors}
+            watch={watch}
+          />
+        )}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default ForgotPasswordScreen;
