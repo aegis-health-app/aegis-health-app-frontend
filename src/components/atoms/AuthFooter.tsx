29870f0e1609fe76bda32b097d8a929d2a3363ba
SPK137 <o.sippakorn@gmail.com>
2022-03-26 16:40:09 +0700

    feat: navigation in AuthFooter

--- src/components/atoms/AuthFooter.tsx
+++ src/components/atoms/AuthFooter.tsx
@@ -1,6 +1,9 @@
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { HStack, ITextProps, Pressable, Text } from 'native-base';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { RootStackParamList } from '../../navigation/types';
 
 export enum AuthType {
   SIGNIN = 'SIGN_IN',
@@ -13,13 +16,20 @@ type TextInputProps = {
 
 const AuthFooter: React.FC<TextInputProps> = ({ page, ...props }) => {
   const { t } = useTranslation();
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
   return (
     <HStack justifyContent="center" my={10} {...props}>
       <Text>
         {page === AuthType.SIGNIN && t('auth.noAccount')}
         {page === AuthType.SIGNUP && t('auth.alreadyHaveAccount')}
       </Text>
-      <Pressable>
+      <Pressable
+        onPress={() =>
+          navigation.replace(
+            page === AuthType.SIGNUP ? 'SignInScreen' : 'SignUpScreen'
+          )
+        }>
         <Text color="blue.600">
           {page === AuthType.SIGNIN && ` ${t('auth.footerSignUp')}`}
           {page === AuthType.SIGNUP && ` ${t('auth.signIn')}`}
