840248281027377c0b65567595270da6f590380a
chuwong35122 <chuwong35122@gmail.com>
2022-03-26 13:12:51 +0700

    feat: add modules and displaying added modules in module manage screen

--- src/screens/ModuleManageScreen.tsx
+++ src/screens/ModuleManageScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { View, Icon, ScrollView } from 'native-base';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { useTranslation } from 'react-i18next';
@@ -6,7 +6,6 @@ import ManageModuleCard from './../components/molecules/ManageModuleCard';
 
 const ModuleManageScreen = () => {
   const { t } = useTranslation();
-  const { moduleList } = useContext(ElderlyContext);
 
   return (
     <ScrollView>
