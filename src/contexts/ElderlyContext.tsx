9169ec60496f8755451529f813d74d6c3a1a8ecb
supat <supatachbeam@gmail.com>
2022-03-27 00:48:57 +0700

    feat: added QR & input code validation

--- src/contexts/ElderlyContext.tsx
+++ src/contexts/ElderlyContext.tsx
@@ -3,12 +3,15 @@ import { getModuleList } from '../utils/module/manage';
 import { getElderlyProfile } from '../utils/elderly/profile';
 import { ElderlyHomeProfile, Module } from './../dto/modules/modules.dto';
 import useAsyncEffect from './../hooks/useAsyncEffect';
+import { getElderlyCode } from '../utils/elderly/code';
 
 export interface ElderlyContextProps {
   elderlyProfile: ElderlyHomeProfile | undefined;
   setElderlyProfile: (val: ElderlyHomeProfile) => void;
   moduleList: Module[];
   setModuleList: (val: Module[]) => void;
+  elderlyCode: string;
+  setElderlyCode: (val: string) => void;
 }
 
 export const ElderlyContext = createContext({} as ElderlyContextProps);
@@ -16,11 +19,16 @@ export const ElderlyContext = createContext({} as ElderlyContextProps);
 const ElderlyContextProvider = ({ ...props }) => {
   const [elderlyProfile, setElderlyProfile] = useState<ElderlyHomeProfile>();
   const [moduleList, setModuleList] = useState<Module[]>([]); // available modules and its name
+  const [elderlyCode, setElderlyCode] = useState<string>('');
 
   //If the user is elderly, get moduleIds and all available modules from the backend.
   //TODO: maybe check if the user is an elderly
   useAsyncEffect(async () => {
     const _elderlyProfile = await getElderlyProfile();
+    const _elderlyCode = await getElderlyCode();
+
+    console.log(_elderlyCode["code"])
+    setElderlyCode(_elderlyCode["code"])
 
     if (_elderlyProfile.listModuleid) {
       _elderlyProfile.listModuleid = [0, ..._elderlyProfile?.listModuleid, 100];
@@ -38,7 +46,9 @@ const ElderlyContextProvider = ({ ...props }) => {
     elderlyProfile,
     setElderlyProfile,
     moduleList,
-    setModuleList
+    setModuleList,
+    elderlyCode,
+    setElderlyCode
   };
 
   return <ElderlyContext.Provider value={value} {...props} />;
--- src/screens/caretakers/InputCodeScreen.tsx
+++ src/screens/caretakers/InputCodeScreen.tsx
@@ -1,5 +1,5 @@
 import { View, Text, Button } from 'native-base';
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../navigation/types';
@@ -8,78 +8,99 @@ import TextInput from '../../components/atoms/TextInput';
 import { useForm, useFormState } from 'react-hook-form';
 import * as Yup from 'yup';
 import { useYupValidationResolver } from '../../hooks/useYupValidationResolver';
+import { client } from '../../config/axiosConfig';
 
 const InputCodeScreen = () => {
-
   const { t } = useTranslation();
 
   const navigation =
-  useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
   const codeSchema = Yup.object({
-    elderlyCode: Yup.string().min(6,'')
-  })
+    elderlyCode: Yup.string().min(6, '')
+  });
+
+  const [elderlyData, setElderlyData] = useState({});
 
   const {
     control,
     formState: { errors },
     watch
-  } = useForm({ resolver:  useYupValidationResolver(codeSchema), mode: "all"});
-
+  } = useForm({ resolver: useYupValidationResolver(codeSchema), mode: 'all' });
 
   // todo: get codes from backend
-  const [codes, setCodes] = useState(['000000','AAAAAA'])
-  const [isCodeValid, setIsCodeValid] = useState(true)
+  const [codes, setCodes] = useState(['000000', 'AAAAAA']);
+  const [isCodeValid, setIsCodeValid] = useState(true);
   const watchElderlyCode = watch();
 
-  const { isValid } = useFormState({control})
+  const { isValid } = useFormState({ control });
 
-  const handleSubmit = () => {
-    if(codes.includes(watchElderlyCode["elderlyCode"])) {
-      navigation.navigate('ConfirmConnectScreen')
-      setIsCodeValid(true)
-    }
-    else setIsCodeValid(false)
-  }
+  const handleSubmit = async () => {
+    const enteredCode = watchElderlyCode["elderlyCode"]
+    await client
+      .get(`/link/elderly/${enteredCode}`)
+      .then(({ data }) => {
+        setElderlyData(data);
+        console.log(elderlyData);
+        navigation.navigate('ConfirmConnectScreen', { info: elderlyData });
+        setIsCodeValid(true)
+      })
+      .catch((err) => {
+        console.log({ err });
+        setIsCodeValid(false)
+      });
+  };
 
   return (
     <View my="2">
-        {/* TODO: add tab */}
-          <Button.Group isAttached justifyContent="center" mb="4">
-            <Button borderWidth="1"
-                borderColor="#1D84DF"
-                backgroundColor="#FAFAFA"
-                _text={{ color: '#1D84DF' }}
-                _pressed={{
-                  borderColor: '#7CC2FF',
-                  _text: { color: '#7CC2FF' }
-                }}
-                borderRadius="0" 
-                width="45%" 
-                onPress={() => navigation.navigate('ConnectElderlyScreen')}>
-              {t('userLink.scanQR')}
-            </Button>
-            <Button borderRadius="0" width="45%" onPress={() => navigation.navigate('InputCodeScreen')}>{t('userLink.enterCode')}</Button>
-        </Button.Group>
-        <View mt={2} paddingX={5}>
+      {/* TODO: add tab */}
+      <Button.Group isAttached justifyContent="center" mb="4">
+        <Button
+          borderWidth="1"
+          borderColor="#1D84DF"
+          backgroundColor="#FAFAFA"
+          _text={{ color: '#1D84DF' }}
+          _pressed={{
+            borderColor: '#7CC2FF',
+            _text: { color: '#7CC2FF' }
+          }}
+          borderRadius="0"
+          width="45%"
+          onPress={() => navigation.navigate('ConnectElderlyScreen')}>
+          {t('userLink.scanQR')}
+        </Button>
+        <Button
+          borderRadius="0"
+          width="45%"
+          onPress={() => navigation.navigate('InputCodeScreen')}>
+          {t('userLink.enterCode')}
+        </Button>
+      </Button.Group>
+      <View mt={2} paddingX={5}>
         <TextInput
-            label={t('userLink.enterElderlyCode')}
-            placeholder='XXXXXX'
-            name="elderlyCode"
-            control={control}
-            errors={errors}
-            mb={2}
-          />
+          label={t('userLink.enterElderlyCode')}
+          placeholder="XXXXXX"
+          name="elderlyCode"
+          control={control}
+          errors={errors}
+          mb={2}
+        />
+      </View>
+      {isCodeValid ? null : (
+        <View flexDir="row">
+          <Text ml="5" color="red.500">
+            {t('userLink.invalidCode')}
+          </Text>
         </View>
-        {isCodeValid? null: 
-          <View flexDir="row">
-            <Text ml="5" color="red.500">
-              {t('userLink.invalidCode')}
-            </Text>
-          </View>}
-        <View my="4" flexDir="row" justifyContent="center">
-            <Button isDisabled={!isValid} width="90.5%" onPress={() => handleSubmit()}>{t('userLink.enterButton')}</Button>
-        </View>
+      )}
+      <View my="4" flexDir="row" justifyContent="center">
+        <Button
+          isDisabled={!isValid}
+          width="90.5%"
+          onPress={() => handleSubmit()}>
+          {t('userLink.enterButton')}
+        </Button>
+      </View>
     </View>
   );
 };
