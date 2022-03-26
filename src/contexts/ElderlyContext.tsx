7a0ed7209ae12422f564cd44e8bc7ca60019aa3d
chuwong35122 <chuwong35122@gmail.com>
2022-03-26 13:36:07 +0700

    chore: remove unused imports

--- src/contexts/ElderlyContext.tsx
+++ src/contexts/ElderlyContext.tsx
@@ -1,9 +1,8 @@
-import React, { useState, createContext, useContext, useEffect } from 'react';
+import React, { useState, createContext } from 'react';
 import { getModuleList } from '../utils/module/manage';
 import { getElderlyProfile } from '../utils/elderly/profile';
 import { ElderlyHomeProfile, Module } from './../dto/modules/modules.dto';
 import useAsyncEffect from './../hooks/useAsyncEffect';
-import { UserContext } from './UserContext';
 
 export interface ElderlyContextProps {
   elderlyProfile: ElderlyHomeProfile | undefined;
@@ -17,17 +16,10 @@ export const ElderlyContext = createContext({} as ElderlyContextProps);
 const ElderlyContextProvider = ({ ...props }) => {
   const [elderlyProfile, setElderlyProfile] = useState<ElderlyHomeProfile>();
   const [moduleList, setModuleList] = useState<Module[]>([]); // available modules and its name
-  const { isElderly } = useContext(UserContext);
-
-  React.useEffect(() => {
-    console.log(elderlyProfile);
-  }, [elderlyProfile]);
 
   //If the user is elderly, get moduleIds and all available modules from the backend.
+  //TODO: maybe check if the user is an elderly
   useAsyncEffect(async () => {
-    // if (isElderly === false) {
-    //   return;
-    // }
     const _elderlyProfile = await getElderlyProfile();
 
     if (_elderlyProfile.listModuleid) {
@@ -40,7 +32,7 @@ const ElderlyContextProvider = ({ ...props }) => {
     _moduleList.unshift({ mname: 'Emergency', moduleid: 0 });
     _moduleList.push({ mname: 'Modules Manage', moduleid: 100 });
     setModuleList(_moduleList);
-  }, [isElderly]);
+  }, []);
 
   const value = {
     elderlyProfile,
