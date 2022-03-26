59b1fda0a617f967f981d7f54e14ca2b77399540
chuwong35122 <chuwong35122@gmail.com>
2022-03-26 20:56:35 +0700

    feat: Caretaker home page UI

--- src/components/organisms/UpComingAlert.tsx
+++ src/components/organisms/UpComingAlert.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import { Text, View } from 'native-base';
+import React, { useContext } from 'react';
+import { HStack, Text, View } from 'native-base';
 import ModuleAlertCard from '../molecules/ModuleAlertCard';
 import { useTranslation } from 'react-i18next';
+import { UserContext } from '../../contexts/UserContext';
+import { TouchableOpacity } from 'react-native';
 
 const UpComingAlert = () => {
   const { t } = useTranslation();
+  const { user } = useContext(UserContext);
+
   return (
-    <View>
-      <Text fontSize="2xl" fontWeight="600" mb={4}>
-        {t('modules.notification')}
-      </Text>
+    <View w="full" mt={6}>
+      <HStack justifyContent="space-between" alignItems="center">
+        <Text fontSize="2xl" fontWeight="600" mb={4}>
+          {t('modules.notification')}
+        </Text>
+        <TouchableOpacity>
+          <Text
+            fontSize="lg"
+            fontWeight="500"
+            mb={4}
+            color="primary.500"
+            underline>
+            {t('modules.viewAll')}
+          </Text>
+        </TouchableOpacity>
+      </HStack>
       <ModuleAlertCard
         moduleName="Lorem Ipsum"
         title="Lorem Ipsum"
