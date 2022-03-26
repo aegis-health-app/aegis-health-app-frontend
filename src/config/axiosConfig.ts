88f2c79fbebef26ab576a6772cad129e8cc5c094
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2022-03-26 22:55:44 +0700

    fix: remove axios config hardcode

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -7,9 +7,7 @@ export const client = Axios.create({
 client.interceptors.request.use(
   async (req) => {
     if (!req.headers?.Authorization) {
-      const token =
-        (await AsyncStorage.getItem('token')) ??
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjI5LCJyb2xlIjoiQ2FyZXRha2VyIiwiaWF0IjoxNjQ4MjM5MTkxLCJleHAiOjE2NDg4NDM5OTF9.KQtL4bc5WYvX8HIwi3_poILyhetouRqPllx6DgzrVmY';
+      const token = (await AsyncStorage.getItem('token')) ?? '';
       if (req.headers) req.headers.Authorization = `Bearer ${token}`;
       return req;
     } else return req;
