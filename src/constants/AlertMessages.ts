a2248a7fe5de59f350ccd10bcdd1b654b159a6e6
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2022-03-26 13:45:16 +0700

    feat: connect profile edit screen

--- src/constants/AlertMessages.ts
+++ src/constants/AlertMessages.ts
@@ -19,5 +19,23 @@ export const AlertMessages = {
     body: i18n.t('changePasswordError.body'),
     primaryButton: i18n.t('changePasswordError.primaryButton'),
     secondaryButton: i18n.t('changePasswordError.secondaryButton')
+  },
+  updateProfileSuccess: {
+    header: i18n.t('updateProfileSuccess.header'),
+    body: i18n.t('updateProfileSuccess.body'),
+    primaryButton: i18n.t('updateProfileSuccess.primaryButton'),
+    secondaryButton: i18n.t('updateProfileSuccess.secondaryButton')
+  },
+  updateProileError: {
+    header: i18n.t('updateProfileError.header'),
+    body: i18n.t('updateProfileError.body'),
+    primaryButton: i18n.t('updateProfileError.primaryButton'),
+    secondaryButton: i18n.t('updateProfileError.secondaryButton')
+  },
+  uploadImageError: {
+    header: i18n.t('uploadImageError.header'),
+    body: i18n.t('uploadImageError.body'),
+    primaryButton: i18n.t('uploadImageError.primaryButton'),
+    secondaryButton: i18n.t('uploadImageError.secondaryButton')
   }
 };
