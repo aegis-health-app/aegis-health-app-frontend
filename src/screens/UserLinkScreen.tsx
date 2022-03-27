15d029953450b28e6e6fc7afc489dcf086fa7900
supat <supatachbeam@gmail.com>
2022-03-27 18:43:33 +0700

    feat: data handling for UserLinkScreen and EditCaretakerScreen

--- src/screens/UserLinkScreen.tsx
+++ src/screens/UserLinkScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import { View, ScrollView, Button, FlatList } from 'native-base';
 import AddButton from '../components/atoms/AddButton';
 import Card from '../components/organisms/UserCard';
@@ -6,36 +6,29 @@ import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigation/types';
 import UserCard from './../components/organisms/UserCard';
+import { ElderlyContext } from '../contexts/ElderlyContext';
 
 const UserLinkScreen = () => {
   // todo: connect with backend's data
-  const [caretakerNumber, setCaretakerNumber] = useState([
-    {
-      dname: 'Supatach Vanichayangkuranont',
-      imageid: 'https://www.beartai.com/wp-content/uploads/2021/08/23.png'
-    },
-    {
-      dname: 'Somying Muangyim',
-      imageid: 'https://www.beartai.com/wp-content/uploads/2021/08/23.png'
-    },
-    {
-      dname: 'Somying Muangyim',
-      imageid: 'https://www.beartai.com/wp-content/uploads/2021/08/23.png'
-    }
-  ]);
+  const {caretakerList, setCaretakerList} = useContext(ElderlyContext);
+  console.log(caretakerList);
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
   return (
     <ScrollView>
       <View bgColor="#FAFAFA">
         <FlatList
-          data={caretakerNumber}
+          data={caretakerList}
           renderItem={({ item }) => (
             <View alignItems="center">
               <UserCard
                 name={item.dname}
+                fullName={`${item.fname} ${item.lname}`}
                 imageId={item.imageid}
                 userIsElderly={false}
+                gender={item.gender}
+                bdate={item.bday}
+                phone={item.phone}
               />
             </View>
           )}
