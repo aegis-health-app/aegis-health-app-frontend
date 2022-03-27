7e026136e2dfe87881db79be4ef2bf60cb70a305
Panoj-k <Panoj.ka@gmail.com>
2022-03-27 17:32:28 +0700

    wip: remove commented code

--- src/dto/PhoneVerificationCode.ts
+++ src/dto/PhoneVerificationCode.ts
@@ -1,9 +1,5 @@
 import * as Yup from 'yup';
 
 export const phoneNumberVerificationCodeSchema = Yup.object({
-<<<<<<< HEAD
   otp: Yup.string().required().length(6)
-=======
-  verificationCode: Yup.string().required('verificationCode').length(6)
->>>>>>> 6daae0e (wip:connect verification code to backend)
 });
