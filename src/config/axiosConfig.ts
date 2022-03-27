6084dd59aaf6c4259acaf05c38013ee0e12358c6
supat <supatachbeam@gmail.com>
2022-03-27 14:56:12 +0700

    fix: removed JWT

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -7,8 +7,7 @@ export const client = Axios.create({
 client.interceptors.request.use(
   async (req) => {
     if (!req.headers?.Authorization) {
-      const token = (await AsyncStorage.getItem('token')) ?? 
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjI4LCJyb2xlIjoiQ2FyZXRha2VyIiwiaWF0IjoxNjQ4MjI1ODMxLCJleHAiOjE2NDg4MzA2MzF9.lHR0SMB7tjGsMQTISm2iE19LeeX9osTBKqqUHeoq8Kk';
+      const token = (await AsyncStorage.getItem('token')) ?? '';
       if (req.headers) req.headers.Authorization = `Bearer ${token}`;
       return req;
     } else return req;
