45efbae843cc6632cb47c81a93df804b699c7384
supat <supatachbeam@gmail.com>
2022-03-28 15:47:51 +0700

    feat: dynamic rendering for UserLinkScreen when new link is formed (on reentering page)

--- src/screens/UserLinkScreen.tsx
+++ src/screens/UserLinkScreen.tsx
@@ -1,50 +1,56 @@
 import React, { useContext } from 'react';
-import { View, ScrollView, Button, FlatList } from 'native-base';
+import { View, Button, FlatList } from 'native-base';
 import AddButton from '../components/atoms/AddButton';
-import Card from '../components/organisms/CaretakerCard';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useIsFocused } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigation/types';
 import UserCard from '../components/organisms/CaretakerCard';
 import { ElderlyContext } from '../contexts/ElderlyContext';
+import useAsyncEffect from '../hooks/useAsyncEffect';
+import { getCaretakerList } from '../utils/elderly/caretakerList';
 
 const UserLinkScreen = () => {
-  
-  const {caretakerList, setCaretakerList} = useContext(ElderlyContext);
-  
+  const isFocused = useIsFocused();
+
+  const { caretakerList, setCaretakerList } = useContext(ElderlyContext);
+
+  useAsyncEffect(async () => {
+    setCaretakerList(await getCaretakerList());
+  }, [isFocused]);
+
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
   return (
-      <View bgColor="#FAFAFA" flex={1}>
-        <FlatList
-          data={caretakerList}
-          renderItem={({ item }) => (
-            <View alignItems="center">
-              <UserCard
-                name={item.dname}
-                fullName={`${item.fname} ${item.lname}`}
-                imageId={item.imageid}
-                userIsElderly={false}
-                gender={item.gender}
-                bdate={item.bday}
-                phone={item.phone}
-                uid={item.uid}
-              />
-            </View>   
-          )}
-          keyExtractor={(_, key) => key.toString()}
-        />
-        <AddButton />
+    <View bgColor="#FAFAFA" flex={1}>
+      <FlatList
+        data={caretakerList}
+        renderItem={({ item }) => (
+          <View alignItems="center">
+            <UserCard
+              name={item.dname}
+              fullName={`${item.fname} ${item.lname}`}
+              imageId={item.imageid}
+              userIsElderly={false}
+              gender={item.gender}
+              bdate={item.bday}
+              phone={item.phone}
+              uid={item.uid}
+            />
+          </View>
+        )}
+        keyExtractor={(_, key) => key.toString()}
+      />
+      <AddButton />
 
-        {/* Placeholder Navigation */}
-        <View flexDir="row" justifyContent="center">
-          <Button
-            width="90%"
-            onPress={() => navigation.navigate('ConnectElderlyScreen')}>
-            Go to Caretaker's Page
-          </Button>
-        </View>
+      {/* Placeholder Navigation */}
+      <View flexDir="row" justifyContent="center">
+        <Button
+          width="90%"
+          onPress={() => navigation.navigate('ConnectElderlyScreen')}>
+          Go to Caretaker's Page
+        </Button>
       </View>
+    </View>
   );
 };
 export default UserLinkScreen;
