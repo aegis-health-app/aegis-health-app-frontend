b246531bb2af0121153e719e10e9d1b36028035c
supat <supatachbeam@gmail.com>
2022-03-28 16:28:32 +0700

    refactor: throw errors

--- src/utils/elderly/code.ts
+++ src/utils/elderly/code.ts
@@ -1,6 +1,10 @@
 import { client } from '../../config/axiosConfig';
 
 export async function getElderlyCode(): Promise<string> {
-  const { data } = await client.get('/link/elderlycode');
-  return data.code;
+  try {
+    const { data } = await client.get('/link/elderlycode');
+    return data.code;
+  } catch (err) {
+    throw Error('Cannot retrieve code');
+  }
 }
