705b84b66b1b3ca7b5eb757c2789cf907b6dba72
supat <supatachbeam@gmail.com>
2022-03-28 11:44:07 +0700

    refactor: removed console.logs

--- src/utils/elderly/displayNames.ts
+++ src/utils/elderly/displayNames.ts
@@ -1,14 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export interface DisplayName {
-  uid: number;
-  newDisplayName: string;
-}
-
-export interface DisplayNameList {
-  DisplayList: DisplayName[];
-}
-
 export const storeDisplayName = async (key: number, value: string) => {
   try {
     await AsyncStorage.setItem(`${key}`, value);
@@ -35,15 +26,13 @@ export const removeDisplayName = async (key: number) => {
   } catch (e) {
     console.log(e);
   }
-
-  console.log('Done.');
 };
 
 export const getAllKeys = async () => {
   try {
     let keys = await AsyncStorage.getAllKeys();
-    return keys
+    return keys;
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-}
+};
