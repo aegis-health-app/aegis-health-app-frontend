1a7d46281968d85d10d9faa30a6c141b72bc9a72
supat <supatachbeam@gmail.com>
2022-03-29 19:27:46 +0700

    wip: data handling for data field

--- src/screens/CustomHealthRecordingScreen.tsx
+++ src/screens/CustomHealthRecordingScreen.tsx
@@ -1,4 +1,13 @@
-import { Box, Button, Icon, Image, ScrollView, Text, View } from 'native-base';
+import {
+  Box,
+  Button,
+  FlatList,
+  Icon,
+  Image,
+  ScrollView,
+  Text,
+  View
+} from 'native-base';
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
@@ -22,10 +31,11 @@ import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigation/types';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { StyleSheet } from 'react-native';
+import useKeyboardOpen from '../hooks/useKeyboardOpen';
 
-interface Fields {
-  fieldName: string;
-  unit: string;
+export interface Fields {
+  fieldName: string | undefined;
+  unit: string | undefined;
 }
 
 interface dictOfFields {
@@ -34,8 +44,10 @@ interface dictOfFields {
 
 const CustomHealthRecordingScreen = () => {
   const [customImage, setCustomImage] = useState<ImagePickerResponse>();
-  const [fieldNumber, setFieldNumber] = useState<number>(0);
-  const [dict, setDict] = useState<dictOfFields>();
+  const [fieldNumber, setFieldNumber] = useState<number[]>([1]);
+  const [fieldCount, setFieldCount] = useState<number>(1);
+  const [dict, setDict] = useState<dictOfFields>({});
+  const [fieldList, setFieldList] = useState<Fields[]>([{ fieldName: undefined, unit: undefined }]);
   const inputSchema = Yup.object({
     title: Yup.string().required(i18n.t('healthRecording.titleBlankError')),
     fieldName: Yup.string().required(i18n.t('healthRecording.fieldBlankError')),
@@ -47,7 +59,8 @@ const CustomHealthRecordingScreen = () => {
   const {
     control,
     formState: { errors },
-    watch
+    watch,
+    reset
   } = useForm({
     resolver: useYupValidationResolver(inputSchema),
     mode: 'onTouched'
@@ -96,105 +109,158 @@ const CustomHealthRecordingScreen = () => {
     );
   };
 
-  // const handleChange = (index: number, val: Fields) => {
-  //   setDict({...dict, id:index, field: val})
-  // }
+  const handleRemove = (id: number) => {
+    console.log(id);
+    setFieldNumber(fieldNumber.filter((ele) => ele !== id));
+  };
 
+  const handleFieldRemove = (id: number) => {
+    const list = [...fieldList];
+    list.splice(id, 1);
+    setFieldList(list);
+  }
+
+  const handleFieldAdd = () => {
+    setFieldList([...fieldList, { fieldName: undefined, unit: undefined }]);
+  };
+
+  const handleDefaultValue = (id: number) => {
+    return dict[fieldNumber[id + 1]];
+  };
+
+  const handleFieldChange = (fieldData: Fields, index) => {
+    const list = [...fieldList];
+    list[index] = fieldData;
+    setFieldList(list);
+  };
+
+  useEffect(() => {
+    console.log(dict);
+  });
   return (
-    <SafeAreaView edges={['left', 'top', 'right']}>
-      <ScrollView>
-        <View mx={4} flexDir="column">
-          <View mt={4}>
-            <Text fontSize={17} fontWeight="bold">
-              {t('healthRecording.generalInformation')}
-            </Text>
+    <View flex={1}>
+      <SafeAreaView edges={['left', 'top', 'right']}>
+        <ScrollView height="85%">
+          <View mx={4} flexDir="column">
+            <View mt={4}>
+              <Text fontSize={17} fontWeight="bold">
+                {t('healthRecording.generalInformation')}
+              </Text>
+            </View>
+            <View mt={3}>
+              <Text fontSize={16} color="#52525B">
+                {t('healthRecording.title')}
+              </Text>
+              <TextInput
+                placeholder={t('healthRecording.title')}
+                name="title"
+                control={control}
+                errors={errors}
+              />
+            </View>
+            {/* Image Part */}
+            <View mt={2}>
+              <Text fontSize={16} color="#52525B">
+                {t('healthRecording.image')}
+              </Text>
+            </View>
+            <View mt={2}>{handleImageSelected()}</View>
+            <View mt={2} flexDir="row" justifyContent="space-between">
+              <Button width="48%" onPress={() => takePicture()}>
+                {t('healthRecording.takeAPicture')}
+              </Button>
+              <Button width="48%" onPress={() => selectPictureFromDevice()}>
+                {t('healthRecording.browse')}
+              </Button>
+            </View>
+            {/* Data Field Part */}
+            <View mt={4}>
+              <Text fontSize={17} fontWeight="bold">
+                {t('healthRecording.dataField')}
+              </Text>
+            </View>
+            {/* Data Field Inputs Part */}
+            <View>
+              {fieldList.map((field, index) => (
+                <View key={index} flexDir="row">
+                  <DataFieldInput
+                    id={fieldNumber[index + 1]}
+                    hasX={true}
+                    defaultValue={field}
+                    onChange={(val: Fields) =>
+                      handleFieldChange(val, index)
+                    }
+                  />
+                  { fieldList.length !== 1 ?
+                  <Icon
+                    style={styles.icon}
+                    ml={3}
+                    as={MaterialIcons}
+                    name="close"
+                    size="6"
+                    color="muted.600"
+                    onPress={() => {
+                      handleFieldRemove(fieldNumber[index + 1]);
+                    }}
+                  />:null
+}
+                </View>
+              ))}
+            </View>
+            <View my={6}>
+              <Button
+                width="100%"
+                size="md"
+                borderWidth="1"
+                borderColor="#1D84DF"
+                backgroundColor="#FAFAFA"
+                _text={{ color: '#1D84DF' }}
+                _pressed={{
+                  borderColor: '#7CC2FF',
+                  _text: { color: '#7CC2FF' }
+                }}
+                onPress={() => {
+                  handleFieldAdd()
+                }}>
+                {t('healthRecording.addField')}
+              </Button>
+            </View>
           </View>
-          <View mt={3}>
-            <Text fontSize={16} color="#52525B">
-              {t('healthRecording.title')}
-            </Text>
-            <TextInput
-              placeholder={t('healthRecording.title')}
-              name="title"
-              control={control}
-              errors={errors}
-            />
-          </View>
-          {/* Image Part */}
-          <View mt={2}>
-            <Text fontSize={16} color="#52525B">
-              {t('healthRecording.image')}
-            </Text>
-          </View>
-          <View mt={2}>{handleImageSelected()}</View>
-          <View mt={2} flexDir="row" justifyContent="space-between">
-            <Button width="48%" onPress={() => takePicture()}>
-              {t('healthRecording.takeAPicture')}
-            </Button>
-            <Button width="48%" onPress={() => selectPictureFromDevice()}>
-              {t('healthRecording.browse')}
-            </Button>
-          </View>
-          {/* Data Field Part */}
-          <View mt={4}>
-            <Text fontSize={17} fontWeight="bold">
-              {t('healthRecording.dataField')}
-            </Text>
-          </View>
-          {/* Data Field Inputs Part */}
-          <View>
-            <DataFieldInput
-              id={1}
-              onChange={(val: Fields) => setDict({ ...dict, 1: val })}
-            />
-            {Array.from(Array(fieldNumber)).map((x, index) => (
-              <View key={index} flexDir="row">
-                <DataFieldInput
-                  id={index + 2}
-                  key={index}
-                  hasX={true}
-                  onChange={(val: Fields) =>
-                    setDict({ ...dict, [index + 2]: val })
-                  }
-                />
-                <Icon
-                  style={styles.icon}
-                  ml={3}
-                  as={MaterialIcons}
-                  name="close"
-                  size="6"
-                  color="muted.600"
-                  onPress={() => console.log('todo: delete')}
-                />
-              </View>
-            ))}
-          </View>
-          <View mt={6}>
-            <Button
-              width="100%"
-              size="md"
-              borderWidth="1"
-              borderColor="#1D84DF"
-              backgroundColor="#FAFAFA"
-              _text={{ color: '#1D84DF' }}
-              _pressed={{
-                borderColor: '#7CC2FF',
-                _text: { color: '#7CC2FF' }
-              }}
-              onPress={() => {
-                setFieldNumber(fieldNumber + 1);
-                console.log(fieldNumber);
-              }}>
-              {t('healthRecording.addField')}
-            </Button>
-          </View>
-          {/* <Button onPress={() => setDict({...dict, 1: {fieldName: 'hi', unit:'banana'}})}>add 1</Button>
-          <Button onPress={() => setDict({...dict, 2: {fieldName: 'ju', unit:'apple'}})}>add 2</Button>
-          <Button onPress={() => setDict({...dict, 1: {fieldName: 'changed', unit:'changed'}})}>change 1</Button> */}
-          <Button onPress={() => console.log(dict)}>log</Button>
+        </ScrollView>
+      </SafeAreaView>
+      {useKeyboardOpen() ? (
+        <View
+          bottom="0"
+          height="15%"
+          width="100%"
+          position="absolute"
+          bgColor="white">
+          <Button
+            isDisabled={true}
+            mx={4}
+            mt={3}
+            onPress={() => console.log(dict)}>
+            {t('healthRecording.create')}
+          </Button>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      ) : (
+        <View
+          bottom="0"
+          height="15%"
+          width="100%"
+          position="absolute"
+          bgColor="white">
+          <Button
+            mx={4}
+            mt={4}
+            onPress={() => {
+              console.log(fieldList);
+            }}>
+            {t('healthRecording.create')}
+          </Button>
+        </View>
+      )}
+    </View>
   );
 };
 
