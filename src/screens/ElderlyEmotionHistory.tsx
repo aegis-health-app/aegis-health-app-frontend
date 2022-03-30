dee3769aa6443c6b242ca90b8cde2c86883de314
chuwong35122 <chuwong35122@gmail.com>
2022-03-30 14:47:02 +0700

    feat: update navigation and header fontWeight

---
+++ src/screens/ElderlyEmotionHistory.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const ElderlyEmotionHistory = () => {
+  return (
+    <View>
+      <Text>ElderlyEmotionHistory</Text>
+    </View>
+  );
+};
+
+export default ElderlyEmotionHistory;
+
+const styles = StyleSheet.create({});
