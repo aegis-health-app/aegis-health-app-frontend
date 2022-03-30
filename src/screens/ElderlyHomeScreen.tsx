5906cc573e4839945f6de5f337dfcf25606f90cf
Panoj-k <Panoj.ka@gmail.com>
2022-03-30 14:56:45 +0700

    wip: create emotion card modal

--- src/screens/ElderlyHomeScreen.tsx
+++ src/screens/ElderlyHomeScreen.tsx
@@ -37,7 +37,6 @@ import FallbackImage from '../components/molecules/FallbackImage';
 import EmotionCard from '../components/organisms/EmotionCard';
 import { flexbox } from 'native-base/lib/typescript/theme/styled-system';
 import moment from 'moment';
-import FallbackImage from '../components/molecules/FallbackImage';
 
 const ProfilePic = require('../assets/images/profile.png');
 
@@ -236,3 +235,23 @@ const ElderlyHomeScreen = () => {
 };
 
 export default ElderlyHomeScreen;
+
+const styles = StyleSheet.create({
+  emotionPicker: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'flex-start',
+    flexDirection: 'row'
+  },
+  emotionButton: {
+    backgroundColor: '#fafafa',
+    borderRadius: 20,
+    padding: 10,
+    margin: 8
+  },
+  pictureArea: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 12
+  }
+});
