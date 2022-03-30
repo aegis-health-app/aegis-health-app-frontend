bd6ab36bf8bdcb9d53a5459d33d0bd2421d8912a
chuwong35122 <chuwong35122@gmail.com>
2022-03-30 15:25:21 +0700

    fix: promise rejection when frontend fetch different user type with its token

--- src/contexts/ElderlyContext.tsx
+++ src/contexts/ElderlyContext.tsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useContext } from 'react';
 import { getModuleList } from '../utils/module/manage';
 import { getElderlyProfile } from '../utils/elderly/profile';
 import { ElderlyHomeProfile, Module } from './../dto/modules/modules.dto';
@@ -6,6 +6,7 @@ import useAsyncEffect from './../hooks/useAsyncEffect';
 import { getElderlyCode } from '../utils/elderly/code';
 import { getCaretakerList } from '../utils/elderly/caretakerList';
 import { User } from '../dto/modules/user.dto';
+import { UserContext } from './UserContext';
 
 export interface ElderlyContextProps {
   elderlyProfile: ElderlyHomeProfile | undefined;
@@ -25,10 +26,12 @@ const ElderlyContextProvider = ({ ...props }) => {
   const [moduleList, setModuleList] = useState<Module[]>([]); // available modules and its name
   const [elderlyCode, setElderlyCode] = useState<string>('');
   const [caretakerList, setCaretakerList] = useState<User[]>([]); // list all caretaker's data
-
+  const { user } = useContext(UserContext);
 
   //If the user is elderly, get moduleIds and all available modules from the backend.
   useAsyncEffect(async () => {
+    if (user?.isElderly === false || !user) return;
+
     const _elderlyProfile = await getElderlyProfile();
     const _elderlyCode = await getElderlyCode();
     const _caretakerList = await getCaretakerList();
@@ -46,7 +49,7 @@ const ElderlyContextProvider = ({ ...props }) => {
     _moduleList.unshift({ mname: 'Emergency', moduleid: 0 });
     _moduleList.push({ mname: 'Modules Manage', moduleid: 100 });
     setModuleList(_moduleList);
-  }, []);
+  }, [user]);
 
   const value = {
     elderlyProfile,
