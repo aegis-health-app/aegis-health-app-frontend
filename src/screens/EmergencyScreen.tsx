3bebb3827d1e60e0a6202767913d1a074e2449a3
SPK137 <o.sippakorn@gmail.com>
2022-04-01 14:13:35 +0700

    feat: add basic elderly emergency screen

--- src/screens/EmergencyScreen.tsx
+++ src/screens/EmergencyScreen.tsx
@@ -1,10 +1,36 @@
-import { Text, View } from 'react-native';
-import React from 'react';
+import { View } from 'react-native';
+import React, { useState } from 'react';
+import EmergencyButton from '../components/organisms/EmergencyButton';
+import { Center, VStack, Text, Button } from 'native-base';
+import { useTranslation } from 'react-i18next';
 
 const EmergencyScreen = () => {
+  const { t } = useTranslation();
+
+  const [isEmergency, setEmergency] = useState<boolean>(false);
+
   return (
     <View>
-      <Text>EmergencyScreen</Text>
+      <VStack minH="full" justifyContent="center">
+        <EmergencyButton
+          isEmergency={isEmergency}
+          setEmergency={setEmergency}
+        />
+        <Center>
+          <Text
+            mt={8}
+            mb={4}
+            fontWeight="semibold"
+            fontSize="24"
+            textAlign="center">
+            {t('emergency.callToAction')}
+          </Text>
+          <Text textAlign="center" fontSize="16">
+            {t('emergency.callToActionDesc')}
+          </Text>
+          <Button onPress={() => setEmergency(false)} />
+        </Center>
+      </VStack>
     </View>
   );
 };
