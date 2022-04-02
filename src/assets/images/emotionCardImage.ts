a11f9c1a25d1636b39de76aeed969e0073328d7e
Panoj-k <Panoj.ka@gmail.com>
2022-04-02 22:09:20 +0700

    feat: add greeting image path picker

---
+++ src/assets/images/emotionCardImage.ts
@@ -0,0 +1,11 @@
+const emotionCardImage = {
+  Monday: require('./tempMonday.png'),
+  Tuesday: require('./picturePlaceholder.png'),
+  Wednesday: require('./scanme.png'),
+  Thursday: require('./sompoch.png'),
+  Friday: require('./somying.png'),
+  Saturday: require('./tempSaturday.png'),
+  Sunday: require('./elderlyCare.png')
+};
+
+export default emotionCardImage;
