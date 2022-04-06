01174c194c1ac705c624bc9b99fa03524470c0e1
Panoj-k <Panoj.ka@gmail.com>
2022-04-06 21:10:13 +0700

    wip: Update image source

--- src/assets/images/emotionCardImage.ts
+++ src/assets/images/emotionCardImage.ts
@@ -1,11 +1,20 @@
 const emotionCardImage = {
-  Monday: require('./tempMonday.png'),
-  Tuesday: require('./picturePlaceholder.png'),
-  Wednesday: require('./scanme.png'),
-  Thursday: require('./sompoch.png'),
-  Friday: require('./somying.png'),
-  Saturday: require('./tempSaturday.png'),
-  Sunday: require('./elderlyCare.png')
+  //English picture
+  MondayEn: require('./tempMonday.png'),
+  TuesdayEn: require('./tempMonday.png'),
+  WednesdayEn: require('./tempMonday.png'),
+  ThursdayEn: require('./tempMonday.png'),
+  FridayEn: require('./tempMonday.png'),
+  SaturdayEn: require('./tempMonday.png'),
+  SundayEn: require('./tempMonday.png'),
+  //Thai picture
+  MondayTh: require('./tempMonday.png'),
+  TuesdayTh: require('./tempMonday.png'),
+  WednesdayTh: require('./tempMonday.png'),
+  ThursdayTh: require('./tempMonday.png'),
+  FridayTh: require('./tempMonday.png'),
+  SaturdayTh: require('./tempMonday.png'),
+  SundayTh: require('./tempMonday.png')
 };
 
 export default emotionCardImage;
