124d1a388ee3f16d5a70f111cd289eec1993661d
chuwong35122 <chuwong35122@gmail.com>
2022-04-06 21:15:12 +0700

    wip: get emotion record history

--- src/dto/modules/emotionRecord.ts
+++
@@ -1,6 +0,0 @@
-export interface EmotionalHistory {
-  date: Date;
-  emotion: Emotion;
-}
-
-export type Emotion = 'NA' | 'HAPPY' | 'NEUTRAL' | 'BAD';
--- src/dto/modules/emotionTracking.dto.ts
+++ src/dto/modules/emotionTracking.dto.ts
@@ -1,3 +1,14 @@
 export interface EmotionTrackingState {
   isEnabled: boolean;
 }
+
+export type Emotion = 'NA' | 'HAPPY' | 'NEUTRAL' | 'BAD';
+export interface EmotionHistory {
+  date: Date;
+  emotionLevel: Emotion;
+}
+
+export interface EmotionHistoryResponse {
+  count: number;
+  records: EmotionHistory[];
+}
