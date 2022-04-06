77dbb4d4f13905fecee55be2a947d24c83600de2
chuwong35122 <chuwong35122@gmail.com>
2022-04-06 20:43:14 +0700

    feat: connect emotion switch to backend, remove error toast

--- src/utils/caretaker/switch.ts
+++ src/utils/caretaker/switch.ts
@@ -1,3 +1,4 @@
+import { EmotionTrackingState } from '../../dto/modules/emotionTracking.dto';
 import { client } from './../../config/axiosConfig';
 
 export async function sendEmotionTrackerOn(uid: number) {
@@ -6,4 +7,11 @@ export async function sendEmotionTrackerOn(uid: number) {
 
 export async function sendEmotionTrackerOff(uid: number) {
   return await client.delete(`/emotion-tracking/${uid}`);
+}
+
+export async function getIsEmotionTrackingOn(
+  uid: number
+): Promise<EmotionTrackingState> {
+  const { data } = await client.get(`/emotion-tracking/${uid}`);
+  return data as EmotionTrackingState;
 }
