3ace945ff039258d2253157e44eac46e1c6513b3
SPK137 <o.sippakorn@gmail.com>
2022-04-06 18:01:14 +0700

    feat: ask for location permission function

--- src/utils/permission.ts
+++ src/utils/permission.ts
@@ -30,3 +30,28 @@ export const requestCameraPermission = async () => {
     }
   }
 };
+
+export const requestLocationPermission = async () => {
+  if (Platform.OS === 'android') {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: i18n.t('locationPermissions.title'),
+          message: i18n.t('locationPermissions.message'),
+          buttonNeutral: i18n.t('locationPermissions.buttonNeutral'),
+          buttonNegative: i18n.t('locationPermissions.buttonNegative'),
+          buttonPositive: i18n.t('cameraPermissions.buttonPositive')
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the location');
+        return granted;
+      } else {
+        console.log('location permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+};
