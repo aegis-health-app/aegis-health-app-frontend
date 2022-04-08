8655337eb5f3fb27512bf7d37fbf719a01e9118c
Varisss <varisthehacker@hotmail.com>
2022-04-09 00:30:00 +0700

    feat: connect health recording with backend

--- src/screens/CreateHealthRecordingsScreen.tsx
+++ src/screens/CreateHealthRecordingsScreen.tsx
@@ -1,58 +1,53 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { ChevronDownIcon, ScrollView, View, VStack } from 'native-base';
 import { useTranslation } from 'react-i18next';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigation/types';
-import images from '../assets/images';
 import HealthRecordingCard from '../components/molecules/HealthRecordingCard';
 import FormSubHeader from '../components/atoms/FormSubHeader';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import { HealthRecording } from '../interfaces/HealthRecording';
+import useAsyncEffect from '../hooks/useAsyncEffect';
+import { UserContext } from '../contexts/UserContext';
+import { client } from '../config/axiosConfig';
+import { CaretakerContext } from '../contexts/CaretakerContext';
 
 const CreateHealthRecordingsScreen = () => {
   const { t } = useTranslation();
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  const { user } = useContext(UserContext);
+  const [templates, setTemplates] = useState<HealthRecording[]>([]);
 
-  const templates: HealthRecording[] = [
-    {
-      hrName: 'healthRecordingsTemplates.bloodPressure',
-      imageId: ''
-    },
-    {
-      hrName: 'healthRecordingsTemplates.bloodSugarLevel',
-      imageId: ''
-    },
-    {
-      hrName: 'healthRecordingsTemplates.heartRate',
-      imageId: ''
-    },
-    {
-      hrName: 'healthRecordingsTemplates.fever',
-      imageId: ''
-    },
-    {
-      hrName: 'healthRecordingsTemplates.hdlldl',
-      imageId: ''
-    },
-    {
-      hrName: 'healthRecordingsTemplates.headache',
-      imageId: ''
-    },
-    {
-      hrName: 'healthRecordingsTemplates.constipation',
-      imageId: ''
-    },
-    {
-      hrName: 'healthRecordingsTemplates.anorexia',
-      imageId: ''
-    },
-    {
-      hrName: 'healthRecordingsTemplates.insomnia',
-      imageId: ''
-    }
-  ];
+  const { currentElderlyUid } = useContext(CaretakerContext);
+
+  useAsyncEffect(async () => {
+    const fetchData = async () => {
+      if (user?.isElderly) {
+        try {
+          const res = await client.get('/healthrecord/getAll/elderly');
+          setTemplates(res.data.listHealthRecord);
+        } catch (err) {
+          console.log(err);
+        }
+      } else {
+        try {
+          const payload = {
+            elderlyuid: currentElderlyUid
+          };
+          const res = await client.post(
+            '/healthrecord/getAll/caretaker',
+            payload
+          );
+          setTemplates(res.data.listHealthRecord);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    };
+    fetchData();
+  }, [user]);
 
   return (
     <>
@@ -66,8 +61,8 @@ const CreateHealthRecordingsScreen = () => {
             <HealthRecordingCard
               key={i}
               backgroundColor="#fff"
-              image={images.selfCare}
-              header={t(template.hrName)}
+              image={template.imageid}
+              hrName={template.hrName}
               handlePress={() => console.log('card pressed')}
             />
           ))}
--- src/screens/HealthRecordingsScreen.tsx
+++ src/screens/HealthRecordingsScreen.tsx
@@ -1,13 +1,11 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Button, ScrollView, View, VStack } from 'native-base';
 import { useTranslation } from 'react-i18next';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigation/types';
-import images from '../assets/images';
 import HealthRecordingCard from '../components/molecules/HealthRecordingCard';
 import { StyleSheet } from 'react-native';
-import { HealthRecording } from '../interfaces/HealthRecording';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import useAsyncEffect from '../hooks/useAsyncEffect';
 import { TourguideContext } from '../contexts/TourguideContext';
@@ -15,49 +13,54 @@ import {
   TourGuideZone,
   useTourGuideController
 } from '../library/rn-multiple-tourguide';
+import { client } from '../config/axiosConfig';
+import { UserContext } from '../contexts/UserContext';
+import { HealthRecording } from '../interfaces/healthRecording';
+import { CaretakerContext } from '../contexts/CaretakerContext';
 
 const HealthRecordingsScreen = () => {
   const { t } = useTranslation();
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  const { user } = useContext(UserContext);
+  const [myTemplates, setMyTemplates] = useState<HealthRecording[]>([]);
 
-  const myTemplates: HealthRecording[] = [
-    {
-      hrName: 'healthRecordingsTemplates.bloodPressure',
-      imageId: ''
-    },
-    {
-      hrName: 'healthRecordingsTemplates.bloodSugarLevel',
-      imageId: ''
-    },
-    {
-      hrName: 'healthRecordingsTemplates.heartRate',
-      imageId: ''
-    },
-    {
-      hrName: 'healthRecordingsTemplates.fever',
-      imageId: ''
-    }
-  ];
+  const { currentElderlyUid } = useContext(CaretakerContext);
+
+  useAsyncEffect(async () => {
+    const fetchData = async () => {
+      if (user?.isElderly) {
+        try {
+          const res = await client.get('/healthrecord/getAll/elderly');
+          setMyTemplates(res.data.listHealthRecord);
+        } catch (err) {
+          console.log(err);
+        }
+      } else {
+        try {
+          const payload = {
+            elderlyuid: currentElderlyUid
+          };
+          const res = await client.post(
+            '/healthrecord/getAll/caretaker',
+            payload
+          );
+          setMyTemplates(res.data.listHealthRecord);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    };
+    fetchData();
+  }, [user]);
 
   // tour guide copied and modified from settings screen
 
   const { showHealthRecordingsTourguide, setShowHealthRecordingsTourguide } =
     useContext(TourguideContext);
-  const {
-    canStart, // a boolean indicate if you can start tour guide
-    start, // a function to start the tourguide
-    stop, // a function  to stopping it
-    eventEmitter, // an object for listening some events
-    tourKey
-  } = useTourGuideController();
+  const { canStart, start, stop, eventEmitter, tourKey } =
+    useTourGuideController();
 
-  /*
-  This useEffect will determine whether the tourguide should be shown
-  The guide should be shown automatically only on the first open of the page
-  The 'viewedSettingsTourguide' is the name of the key in AsyncStorage that stores
-  whether this specific tour guide has been viewed yet. If viewd, it returns true.
-  */
   useAsyncEffect(async () => {
     const fetchData = async () => {
       const result = await AsyncStorage.getItem(
@@ -69,18 +72,10 @@ const HealthRecordingsScreen = () => {
     setShowHealthRecordingsTourguide(shouldShow);
   }, [AsyncStorage, showHealthRecordingsTourguide]);
 
-  /*
-  This useEffect will start the tour guide when the component is ready
-  and the condition is met.
-  */
   useEffect(() => {
     if (canStart && showHealthRecordingsTourguide && start) start();
   }, [canStart, showHealthRecordingsTourguide]);
 
-  /*
-  This useEffect will allow you to run script when a specific event
-  of the tourguide is triggered.
-  */
   useEffect(() => {
     eventEmitter?.on('stop', async () => {
       setShowHealthRecordingsTourguide(false);
@@ -123,8 +118,8 @@ const HealthRecordingsScreen = () => {
                 <HealthRecordingCard
                   key={i}
                   backgroundColor="#fff"
-                  image={images.selfCare}
-                  header={t(template.hrName)}
+                  image={template.imageid}
+                  hrName={template.hrName}
                   handlePress={() => {
                     stop();
                     console.log('card pressed');
@@ -135,8 +130,8 @@ const HealthRecordingsScreen = () => {
               <HealthRecordingCard
                 key={i}
                 backgroundColor="#fff"
-                image={images.selfCare}
-                header={t(template.hrName)}
+                image={template.imageid}
+                hrName={t(template.hrName)}
                 handlePress={() => console.log('card pressed')}
               />
             )}
