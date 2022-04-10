d3f0bfad30f5ec3fd2da5e523950adc50968030f
SPK137 <o.sippakorn@gmail.com>
2022-04-11 00:38:40 +0700

    feat: dedicated alert for emergency

---
+++ src/components/organisms/EmergencyAlert.tsx
@@ -0,0 +1,70 @@
+import React, { useCallback } from 'react';
+import { AlertDialog, Button, Text } from 'native-base';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../../navigation/types';
+import { EmergencyInfo } from '../../screens/EmergencyInfoScreen';
+import { useTranslation } from 'react-i18next';
+
+export enum AlertType {
+  ERROR = 'ERROR',
+  SUCCESS = 'SUCCESS',
+  INFO = 'INFO'
+}
+
+type EmergencyAlertProps = {
+  isOpen: boolean;
+  close: () => void;
+  cancel?: () => void;
+  emergencyInfo: EmergencyInfo;
+};
+
+const EmergencyAlert: React.FC<EmergencyAlertProps> = ({
+  isOpen,
+  close,
+  cancel,
+  emergencyInfo
+}) => {
+  const { t } = useTranslation();
+
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+  const showEmergencyInfoScreen = useCallback(() => {
+    navigation.push('EmergencyInfoScreen', { info: emergencyInfo });
+  }, [emergencyInfo]);
+
+  const cancelRef = React.useRef(null);
+  return (
+    <AlertDialog
+      leastDestructiveRef={cancelRef}
+      isOpen={isOpen}
+      onClose={cancel ?? close}>
+      <AlertDialog.Content>
+        <AlertDialog.CloseButton />
+        <AlertDialog.Header background="aegis.danger">
+          <Text color="white">{t('emergencyNotification.header')}</Text>
+        </AlertDialog.Header>
+        <AlertDialog.Body>
+          {t('emergencyNotification.body', { name: emergencyInfo.name })}
+        </AlertDialog.Body>
+        <AlertDialog.Footer>
+          <Button.Group space={2}>
+            <Button
+              variant="unstyled"
+              colorScheme="coolGray"
+              onPress={cancel}
+              ref={cancelRef}>
+              {t('emergencyNotification.secondaryButton')}
+            </Button>
+            <Button colorScheme="danger" onPress={showEmergencyInfoScreen}>
+              {t('emergencyNotification.primaryButton')}
+            </Button>
+          </Button.Group>
+        </AlertDialog.Footer>
+      </AlertDialog.Content>
+    </AlertDialog>
+  );
+};
+
+export default EmergencyAlert;
