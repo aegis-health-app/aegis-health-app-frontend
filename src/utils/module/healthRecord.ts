846b77120f583e20949af8bc66f7d6e707c60cab
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2022-04-11 18:21:15 +0700

    feat: adjust health analytics table label scale

--- src/utils/module/healthRecord.ts
+++ src/utils/module/healthRecord.ts
@@ -1,11 +1,26 @@
 import moment from 'moment';
+import { TimeFrame } from '../../constants/HealthRecordingConstants';
 import { HealthRecordData } from '../../interfaces/healthRecording';
 
-export function getPeriodLable(data: HealthRecordData[]) {
+export function getPeriodLable(data: HealthRecordData[], timeFrame: TimeFrame) {
   const LABEL_COUNT = 6;
   const interpolate = Math.ceil(data.length / LABEL_COUNT);
   const labels: string[] = [];
 
+  if (timeFrame === TimeFrame.YEAR || timeFrame === TimeFrame.ALL_TIME) {
+    data.forEach((val, index) => {
+      if (
+        index === 0 ||
+        index === data.length - 1 ||
+        index === Math.round((data.length - 1) / 2)
+      ) {
+        labels.push(moment(val.dateTime).format('DD/MM/YY') + '        ');
+      } else {
+        return labels.push('');
+      }
+    });
+    return labels;
+  }
   data.forEach((val, index) => {
     if (index % interpolate === 0) {
       labels.push(moment(val.dateTime).format('DD/MM'));
