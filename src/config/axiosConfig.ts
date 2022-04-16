b6bf87aedd313a960d49324880fdecba4144c02a
chuwong35122 <chuwong35122@gmail.com>
2022-04-16 20:28:38 +0700

    feat: rename MemoryRecallScreen screen

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -7,9 +7,7 @@ export const client = Axios.create({
 client.interceptors.request.use(
   async (req) => {
     if (!req.headers?.Authorization) {
-      const token =
-        (await AsyncStorage.getItem('token')) ??
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEwMywicm9sZSI6IkVsZGVybHkiLCJpYXQiOjE2NTAwOTIyODAsImV4cCI6MTY1MjY4NDI4MH0.IItB0jJ2jNj1xwf9dV6-ZPnmGTdR-IIP-MkPAj-TgQg';
+      const token = (await AsyncStorage.getItem('token')) ?? '';
       if (req.headers) req.headers.Authorization = `Bearer ${token}`;
       return req;
     } else return req;
