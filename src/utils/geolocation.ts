55cf28cde79c815017b90e76b1b142652a63d548
SPK137 <o.sippakorn@gmail.com>
2022-04-20 14:38:05 +0700

    feat: add reverse geocoding

--- src/utils/geolocation.ts
+++ src/utils/geolocation.ts
@@ -1,5 +1,6 @@
 import { Platform, Linking, PermissionsAndroid } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
+import Geocoder from 'react-native-geocoding';
 import { requestLocationPermission } from './permission';
 
 export interface Geolocation {
@@ -39,4 +40,17 @@ export const getCurrentLocation = async (successFunction) => {
       { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
     );
   } else return coords;
+};
+
+export const initGeocoding = () => {
+  Geocoder.init(process.env.GOOGLE_MAPS_API_KEY ?? '');
+};
+
+export const reverseGeocode = async (lat: number, lng: number) => {
+  const address = await Geocoder.from({
+    lat,
+    lng
+  });
+
+  return address.plus_code.compound_code;
 };
