6d88ec87826be18632e02146b8942ed4008fca8e
SPK137 <o.sippakorn@gmail.com>
2022-04-20 14:40:45 +0700

    feat: add reverse geocode when sending emergency signal

--- src/utils/user/notification.ts
+++ src/utils/user/notification.ts
@@ -1,4 +1,5 @@
 import { client } from '../../config/axiosConfig';
+import { reverseGeocode } from '../geolocation';
 
 export const registerFCMToken = async (token) => {
   return await client.post('notification/register-device', {
@@ -7,9 +8,11 @@ export const registerFCMToken = async (token) => {
 };
 
 export const sendEmergencySignal = async ({ latitude, longtitude }) => {
+  const address = await reverseGeocode(latitude, longtitude);
+
   return await client.post('notification/emergency', {
     latitude,
     longtitude,
-    address: 'string'
+    address
   });
 };
