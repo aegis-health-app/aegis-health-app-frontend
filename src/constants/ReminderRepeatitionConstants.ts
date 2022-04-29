9a715c91fbb0696fcb77b0c296d92afb3ee7e79f
SPK137 <o.sippakorn@gmail.com>
2022-04-29 10:36:42 +0700

    feat: constant for i18m of recursion

---
+++ src/constants/ReminderRepeatitionConstants.ts
@@ -0,0 +1,32 @@
+export const ReminderRepeatitionPattern = [
+  {
+    label: 'doesNotRepeat',
+    value: 'doesNotRepeat'
+  },
+  {
+    label: 'everyday',
+    value: 'everyday'
+  },
+  {
+    label: 'everyweek',
+    value: 'everyweek'
+  },
+  {
+    label: 'everymonth',
+    value: 'everymonth'
+  },
+  {
+    label: 'everyyear',
+    value: 'everyyear'
+  },
+  {
+    label: 'custom',
+    value: 'custom'
+  }
+];
+
+export const ReminderInfoScreenRecursion = {
+  EVERY_DAY: 'reminder.dayRecursion',
+  EVERY_WEEK: 'reminder.weekRecursion',
+  EVERY_MONTH: 'reminder.monthRecursion'
+};
